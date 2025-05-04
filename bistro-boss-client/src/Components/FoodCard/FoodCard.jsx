f096f94ac82e7e1a0dc1a9682da62c51949fda1b
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-05-04 22:27:44 +0600

    final project M4V3 done

--- bistro-boss-client/src/Components/FoodCard/FoodCard.jsx
+++ bistro-boss-client/src/Components/FoodCard/FoodCard.jsx
@@ -1,24 +1,71 @@
-import React from 'react'
+import React from "react";
+import useAuth from "../Hooks/useAuth";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const FoodCard = ({item}) => {
-    const {name, recipe, image,  price} = item || {};
+const FoodCard = ({ item }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { name, recipe, image, price, _id } = item || {};
+  const { user } = useAuth();
+  const handleAddToCart = (food) => {
+    if (user && user?.email) {
+      const cartItem = {
+        menuId: _id,
+        email: user?.email,
+        name,
+        image,
+        price,
+      };
+      axios.post("http://localhost:5000/carts", cartItem)
+      .then(res=>{
+        if(res.data.insertedId){
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${name} added to your cart`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      })
+    } else {
+      Swal.fire({
+        title: "You are not Logged in",
+        text: "Please Login first to add to the Cart",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, login!",
+      }).then((result) => {
+        navigate("/login", { state: { from: location } });
+      });
+    }
+  };
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src={image}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body text-center">
-    <p className='bg-gray-800 absolute top-3 right-9 px-4 py-1  text-white'>{price}</p>
-    <h2 className="text-2xl font-semibold">{name}</h2>
-    <p>{recipe}</p>
-    <div className="card-actions justify-center">
-      <button className="btn text-[#BB8506] border-0 border-b-4 rounded-lg px-9 py-5 border-[#BB8506] bg-[#E8E8E8]">Add to Cart</button>
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <div className="card-body text-center">
+        <p className="bg-gray-800 absolute top-3 right-9 px-4 py-1  text-white">
+          {price}
+        </p>
+        <h2 className="text-2xl font-semibold">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions justify-center">
+          <button
+            onClick={() => handleAddToCart(item)}
+            className="btn text-[#BB8506] border-0 border-b-4 rounded-lg px-9 py-5 border-[#BB8506] bg-[#E8E8E8]"
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default FoodCard
+export default FoodCard;
--- bistro-boss-client/src/Shared/Navbar/Navbar.jsx
+++ bistro-boss-client/src/Shared/Navbar/Navbar.jsx
@@ -1,65 +1,100 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../../Providers/AuthProvider'
-import Swal from 'sweetalert2';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
+import Swal from "sweetalert2";
+import { IoCartOutline } from "react-icons/io5";
 
 const Navbar = () => {
-  const {user,signOutUser} = useContext(AuthContext);
+  const { user, signOutUser } = useContext(AuthContext);
   console.log(user);
-  const handleLogOut = ()=>{
-    signOutUser()
+  const handleLogOut = () => {
+    signOutUser();
     Swal.fire({
       position: "top-end",
       icon: "success",
       title: "Log Out Successfully",
       showConfirmButton: false,
-      timer: 1500
+      timer: 1500,
     });
-  }
+  };
 
-    const linkOptions = <>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/menu">Menu</Link></li>
-        <li><Link to="/order/salads">Order</Link></li>
-        <li><Link to="/secret">Secret</Link></li>
-        
-        {
-          user ? 
-          <>
-            <button onClick={handleLogOut} className="btn text-white btn-error">Log Out</button>
-          </> : 
-          <>
-            <li><Link to="/login">Login</Link></li>
-          </>
-        }
+  const linkOptions = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/menu">Menu</Link>
+      </li>
+      <li>
+        <Link to="/order/salads">Order</Link>
+      </li>
+      <li>
+        <Link to="/secret">Secret</Link>
+      </li>
+      <li>
+        <Link to="/">
+          <button className="btn">
+          <IoCartOutline />
+          <div className="badge badge-sm badge-secondary">+0</div>
+          </button>
+        </Link>
+      </li>
+
+      {user ? (
+        <>
+          <button onClick={handleLogOut} className="btn text-white btn-error">
+            Log Out
+          </button>
+        </>
+      ) : (
+        <>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </>
+      )}
     </>
-
+  );
 
   return (
     <div className="navbar fixed z-10 text-white bg-black/30 max-w-screen-xl mx-auto shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {linkOptions}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        {linkOptions}
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{linkOptions}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-        {linkOptions}
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
